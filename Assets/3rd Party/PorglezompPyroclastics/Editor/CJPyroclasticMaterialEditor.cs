43c510831128a5d900b24334c2d6fe112dcc5c16
Shaotastic <aaron.bfbs@gmail.com>
2021-06-22 20:17:26 -0400

    Revert "First commit"
    This reverts commit e0cb687602f0e7d24939f41716ada87455ba6cf9.

--- Assets/3rd Party/PorglezompPyroclastics/Editor/CJPyroclasticMaterialEditor.cs
+++
@@ -1,9 +0,0 @@
-using UnityEditor;
-
-public class CJPyroclasticMaterialEditor : MaterialEditor
-{
-    public override void OnInspectorGUI()
-    {
-        EditorGUILayout.LabelField("Edit the properties using the ExplosionMat script.");
-    }
-}
